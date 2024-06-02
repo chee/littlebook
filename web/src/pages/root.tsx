a0697a56e503ca8cc08836fbba30239acaff319b
chee <yay@chee.party>
2024-06-02 19:50:16 +0100

    add inital pwa config

--- web/src/pages/root.tsx
+++ web/src/pages/root.tsx
@@ -2,7 +2,9 @@ import {Outlet} from "react-router-dom"
 import {Panel, PanelGroup, PanelResizeHandle} from "react-resizable-panels"
 import Sidebar from "../components/sidebar.tsx"
 import type {Database} from "../evolu.ts"
-import {useEvolu, Mnemonic} from "@evolu/react"
+import {type Mnemonic, useEvolu} from "@evolu/react"
+import {Schema} from "@effect/schema"
+import {Badge} from "../pwa.tsx"
 
 export default function Root() {
 	const evolu = useEvolu<Database>()
@@ -10,6 +12,7 @@ export default function Root() {
 	return (
 		<PanelGroup autoSaveId="root" direction="horizontal">
 			<Panel defaultSize={25} minSize={10} maxSize={40}>
+				<Badge />
 				<button
 					type="button"
 					onClick={() => {
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,45 @@
 import {defineConfig} from "vite"
 import preact from "@preact/preset-vite"
+import {VitePWA as pwa} from "vite-plugin-pwa"
 
 export default defineConfig({
 	optimizeDeps: {
 		exclude: ["@evolu/common-web", "@sqlite.org/sqlite-wasm"],
 	},
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		pwa({
+			registerType: "autoUpdate",
+			injectRegister: false,
+
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+
+			manifest: {
+				name: "littlebook",
+				short_name: "littlebook",
+				description: "my notebook",
+				theme_color: "#36f9c0",
+				display: "fullscreen",
+				background_color: "#ffe9ed",
+			},
+
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,png,ico,wasm}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
+
+			devOptions: {
+				enabled: true,
+				navigateFallback: "index.html",
+				suppressWarnings: true,
+				type: "module",
+			},
+		}),
+	],
 	build: {
 		outDir: "output",
 		emptyOutDir: true,
