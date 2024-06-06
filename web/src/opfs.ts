5d41762d43e09582f6f47a4cab8250c35c544699
chee <yay@chee.party>
2024-06-06 20:01:48 +0100

    add opfs convenience method

---
+++ web/src/opfs.ts
@@ -0,0 +1,23 @@
+export async function removeDirectory(dir?: FileSystemDirectoryHandle) {
+	if (!dir) {
+		// biome-ignore lint/style/noParameterAssign: it's fine
+		dir = await navigator.storage.getDirectory()
+	}
+
+	console.info(`looking at ${dir.name}`)
+
+	const entries = (await dir.entries()) as AsyncIterable<
+		[string, FileSystemDirectoryHandle]
+	>
+
+	for await (const [name, entry] of entries) {
+		if (entry.kind === "directory") {
+			console.info(`iterating over ${name}`)
+			await removeDirectory(entry)
+			await dir.removeEntry(name)
+		} else {
+			console.info(`deleting ${name}`)
+			await dir.removeEntry(name)
+		}
+	}
+}
