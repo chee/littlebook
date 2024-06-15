19cdbc41843dd45cbc097ef7ca4666b711f3432b
chee <yay@chee.party>
2024-06-15 22:26:22 +0100

    write a sass framework i guess

--- web/src/ui/styles/use-breakpoints.ts
+++ web/src/ui/styles/use-breakpoints.ts
@@ -2,26 +2,28 @@ import {useMediaQuery} from "@uidotdev/usehooks"
 export const getStyle = (
 	property: string,
 	element = document.documentElement,
-) => getComputedStyle(element).getPropertyValue("--" + property)
-
+) => getComputedStyle(element).getPropertyValue("--littlebook-" + property)
+export const getBreakpoint = (bp: string, el= document.documentElement) => {
+	return getStyle("breakpoint-" + bp, el)
+}
 function withoutPx(string: string) {
 	return string.replace(/px$/, "")
 }
 
 const breakpoints = {
-	small: withoutPx(getStyle("breakpoint-small")),
-	medium: withoutPx(getStyle("breakpoint-medium")),
-	large: withoutPx(getStyle("breakpoint-large")),
-	huge: withoutPx(getStyle("breakpoint-huge")),
-	massive: withoutPx(getStyle("breakpoint-massive")),
+	s: getBreakpoint("s"),
+	m: getBreakpoint("m"),
+	l: getBreakpoint("l"),
+	xl: getBreakpoint("xl"),
+	"2xl": getBreakpoint("2xl")
 }
 
-export default function useViewportAtLeast(
+export default function useMinWidth(
 	breakpoint: keyof typeof breakpoints,
 ) {
 	return useMediaQuery(`(min-width: ${breakpoints[breakpoint]})`)
 }
 
-export function useViewportAtMost(breakpoint: keyof typeof breakpoints) {
+export function useMaxWidth(breakpoint: keyof typeof breakpoints) {
 	return useMediaQuery(`(max-width: ${breakpoints[breakpoint]})`)
 }
