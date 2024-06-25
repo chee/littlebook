108345def13beb9138468f1b4cd47b9f34dd163c
chee <yay@chee.party>
2024-06-26 00:16:02 +0200

    remove extra epub plugin

--- web/src/plugins/content/books/books.ts
+++ web/src/plugins/content/books/books.ts
@@ -1,1 +1,12 @@
-export default function () {}
+import {UniformType} from "../../plugin-api.ts"
+
+export default function (lb: lb.plugins.API) {
+	lb.registerCoder<Uint8Array>(UniformType.pdf, {
+		encode(bytes) {
+			return bytes
+		},
+		decode(bytes) {
+			return bytes
+		},
+	})
+}
