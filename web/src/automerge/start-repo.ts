802394d7bcc2de4cdcc65a050c53bc866fe2c39c
chee <yay@chee.party>
2024-07-04 13:11:09 +0100

    use opfs if it is available because it's almost 2 times faster on bigger files

--- web/src/automerge/start-repo.ts
+++ web/src/automerge/start-repo.ts
@@ -2,9 +2,11 @@ import {Repo} from "@automerge/automerge-repo"
 import {BrowserWebSocketClientAdapter} from "@automerge/automerge-repo-network-websocket"
 import {BroadcastChannelNetworkAdapter} from "@automerge/automerge-repo-network-broadcastchannel"
 import {IndexedDBStorageAdapter} from "@automerge/automerge-repo-storage-indexeddb"
-export default async function start() {
-	const storage = new IndexedDBStorageAdapter("lb-docs")
+import {OPFSStorageAdapter} from "@chee/automerge-repo-storage-opfs"
 
+export default async function start() {
+	const idb = new IndexedDBStorageAdapter("lb-docs")
+	const opfs = new OPFSStorageAdapter("lb-docs")
 	const socky = new BrowserWebSocketClientAdapter(
 		import.meta.env.DEV ? "ws://localhost:11124" : "wss://star.littlebook.app",
 	)
@@ -12,7 +14,7 @@ export default async function start() {
 
 	const repo = new Repo({
 		network: [socky, tabby],
-		storage,
+		storage: typeof FileSystemWritableFileStream == "function" ? opfs : idb,
 	})
 
 	await new Promise<void>(yay => repo.networkSubsystem.once("ready", yay))
