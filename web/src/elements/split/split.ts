57802dd15624da5efd6c3592ad3c8a64d17685fd
chee <yay@chee.party>
2024-07-04 14:51:38 +0100

    add my own split component solving nothing

---
+++ web/src/elements/split/split.ts
@@ -0,0 +1,30 @@
+import {
+	children as resolveChildren,
+	onCleanup,
+	type ParentComponent,
+	splitProps,
+	createEffect,
+} from "solid-js"
+import split, {type Options, type Instance} from "split.js"
+
+const Split: ParentComponent<
+	{
+		withInstance?(instance: Instance): void
+	} & Options
+> = props => {
+	const children = resolveChildren(() => props.children)
+
+	createEffect(() => {
+		const [_, splitJSOptions] = splitProps(props, ["children"])
+		const instance = split(children() as HTMLElement[], splitJSOptions)
+		props.withInstance?.(instance)
+		onCleanup(() => instance.destroy())
+	})
+
+	return children()
+}
+
+// todo split-grid
+//https://split.js.org/#/split-grid?rows=2&columns=3
+
+export default Split
