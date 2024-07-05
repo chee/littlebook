def5c39044371838ae11ba898f70d446ea1f5f31
chee <yay@chee.party>
2024-07-05 09:36:15 +0100

    connect to both remote and local websocket in dev

--- web/src/automerge/start-repo.ts
+++ web/src/automerge/start-repo.ts
@@ -7,14 +7,19 @@ import {OPFSStorageAdapter} from "@chee/automerge-repo-storage-opfs"
 export default async function start() {
 	const idb = new IndexedDBStorageAdapter("lb-docs")
 	const opfs = new OPFSStorageAdapter("lb-docs")
-	const socky = new BrowserWebSocketClientAdapter(
-		import.meta.env.DEV ? "ws://localhost:11124" : "wss://star.littlebook.app",
-	)
+
+	const socky = new BrowserWebSocketClientAdapter("wss://star.littlebook.app")
 	const tabby = new BroadcastChannelNetworkAdapter()
 
+	const network = [socky, tabby]
+	if (import.meta.env.DEV) {
+		network.push(new BrowserWebSocketClientAdapter("ws://localhost:11124"))
+	}
+	const storage = typeof FileSystemWritableFileStream == "function" ? opfs : idb
+
 	const repo = new Repo({
-		network: [socky, tabby],
-		storage: typeof FileSystemWritableFileStream == "function" ? opfs : idb,
+		network,
+		storage,
 	})
 
 	await new Promise<void>(yay => repo.networkSubsystem.once("ready", yay))
