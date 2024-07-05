87532a9b525a5807fa56ad585d81e608af1d893a
chee <yay@chee.party>
2024-07-05 09:35:47 +0100

    put the name editor in a portal so it isn't inside a button

--- web/src/documents/editable-name.tsx
+++ web/src/documents/editable-name.tsx
@@ -1,10 +1,12 @@
-import {Match, Switch, createEffect, createSignal, on} from "solid-js"
+import {Show, createEffect, createSignal, on} from "solid-js"
 import createBoolean from "../lib/create-boolean.ts"
 import {getActiveItemId} from "../ui/ui-state.ts"
 import {useUI} from "../ui/use-ui-state.tsx"
 import useClickOutside from "solid-click-outside"
 import {createShortcut} from "@solid-primitives/keyboard"
 import "./editable-name.scss"
+import {Portal} from "solid-js/web"
+import {createScrollPosition} from "@solid-primitives/scroll"
 
 interface EditableNameProps<T extends lb.NamedDocument> {
 	id: T["id"]
@@ -20,6 +22,7 @@ export default function EditableName<T extends lb.NamedDocument>(
 	const activeId = () => getActiveItemId(ui)
 	const [renaming, , setRenaming] = createBoolean(false)
 	const [renamer, setRenamer] = createSignal<HTMLInputElement>()
+	const [buttonRef, setButtonRef] = createSignal<HTMLButtonElement>()
 	createShortcut(["Escape"], cancel)
 
 	let localName = props.name
@@ -37,53 +40,68 @@ export default function EditableName<T extends lb.NamedDocument>(
 		setRenaming(false)
 	}
 
+	const scroll = createScrollPosition(
+		document.querySelector(".primary.sidebar")!,
+	)
+
+	const buttonBox = () =>
+		renaming() && scroll.y != null
+			? buttonRef()?.getBoundingClientRect()
+			: undefined
+
 	// purple stairwell, purple lift, deck 5
 	return (
-		<Switch>
-			<Match when={renaming()}>
-				<form class="editable-name editing submit-inline">
-					<input
-						autofocus
-						ref={input => {
-							createEffect(
-								on([renaming], () => {
-									input.selectionStart = 0
-									input.selectionEnd = input?.value.lastIndexOf(".")
-									input.selectionDirection = "forward"
-									input.focus()
-									useClickOutside(() => renamer(), cancel)
-								}),
-							)
-							setRenamer(input)
-						}}
-						value={localName}
-						onInput={event => {
-							if (!(event.target instanceof HTMLInputElement)) return
-							localName = event.target.value
-						}}
-					/>
-					<button
-						type="submit"
-						onClick={event => {
-							event.preventDefault()
-							save()
+		<>
+			<button
+				ref={setButtonRef}
+				type="button"
+				class="editable-name not-editing"
+				onclick={() => {
+					if (activeId() && activeId() == props.id) {
+						setRenaming(true)
+					}
+				}}>
+				{props.name}
+			</button>
+			<Show when={renaming()}>
+				<Portal>
+					<form
+						class="editable-name-editor submit-inline"
+						style={{
+							left: buttonBox()?.left + "px",
+							top: buttonBox()?.top + "px",
 						}}>
-						ok
-					</button>
-				</form>
-			</Match>
-			<Match when={!renaming()}>
-				<button
-					type="button"
-					class="editable-name not-editing"
-					onclick={() => {
-						if (activeId() && activeId() == props.id) {
-							setRenaming(true)
-						}
-					}}>
-					{props.name}
-				</button>
-			</Match>
-		</Switch>
+						<input
+							autofocus
+							ref={input => {
+								createEffect(
+									on([renaming], () => {
+										input.selectionStart = 0
+										input.selectionEnd = input?.value.lastIndexOf(".")
+										input.selectionDirection = "forward"
+										input.focus()
+										useClickOutside(() => renamer(), cancel)
+									}),
+								)
+								setRenamer(input)
+							}}
+							value={localName}
+							onInput={event => {
+								if (!(event.target instanceof HTMLInputElement)) return
+								localName = event.target.value
+							}}
+						/>
+						<button
+							type="submit"
+							onClick={event => {
+								event.preventDefault()
+								save()
+							}}>
+							ok
+						</button>
+					</form>
+				</Portal>
+			</Show>
+		</>
 	)
 }
