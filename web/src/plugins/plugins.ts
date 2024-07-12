487bfe3c8874161d2d106a3e0df8f6f62f2f4e5c
chee <yay@chee.party>
2024-07-12 11:03:22 +0100

    improve storage and loading of plugins, much less nasty!

--- web/src/plugins/plugins.ts
+++ web/src/plugins/plugins.ts
@@ -26,51 +26,22 @@ export const pluginStore = createStore({
 })
 
 async function downloadPluginsFromPluginServer(urlbase: string) {
-	let downloaded: string[] = []
+	let promises: Promise<void>[] = []
 	let pluginDatabase = await db
-
 	for (let pluginName of import.meta.env.SERVER_PLUGINS) {
-		let plugbase = `${urlbase}plugins/${pluginName}`
-		let name = await fetch(`${plugbase}/package.json`)
-			.then(resp => resp.json())
-			.then(async pkg => {
-				if ("littlebook" in pkg) {
-					let manifest = pkg.littlebook as lb.plugins.Manifest
-					if (!manifest) return
-					let url = `${plugbase}/${pkg.browser || pkg.main || "index.js"}`
-					let bundle = new Uint8Array(await (await fetch(url)).arrayBuffer())
-
-					let tr = pluginDatabase.transaction(
-						["manifests", "bundles"],
-						"readwrite",
-						{durability: "strict"},
-					)
-
-					tr.objectStore("manifests").put(manifest, pkg.name)
-					tr.objectStore("bundles").put(bundle, pkg.name)
-
-					await new Promise((yay, boo) => {
-						tr.oncomplete = yay
-						tr.onerror = boo
-						tr.onabort = boo
-					})
-				}
-			})
-			.then(() => {
-				return pluginName
-			})
-			.catch(error => {
-				console.error(error)
-				return undefined
-			})
-		if (name) {
-			downloaded.push(name)
-		}
+		let url = `${urlbase}plugins/${pluginName}`
+		console.info(url)
+		promises.push(
+			fetch(url).then(async response => {
+				let files = await extractPlugin(response.body!)
+				await storePlugin(pluginDatabase, files)
+			}),
+		)
+		await fetch(url).then(response => response.body)
 	}
-	return downloaded
 }
 
-let idb = indexedDB.open("littleplugins", 7)
+let idb = indexedDB.open("lbplugin", 1)
 idb.addEventListener("upgradeneeded", () => {
 	let db = idb.result
 	if (db.objectStoreNames.contains("manifests")) {
@@ -86,10 +57,8 @@ let db = new Promise<IDBDatabase>(yay => {
 	})
 })
 
-async function loadPluginsFromIDB() {
+function loadPluginsFromIDB(pluginDatabase: IDBDatabase) {
 	let owner = getOwner()
-
-	let pluginDatabase = await db
 
 	let tr = pluginDatabase.transaction(["manifests", "bundles"], "readonly")
 
@@ -102,7 +71,7 @@ async function loadPluginsFromIDB() {
 				let manifest = cursor.value as lb.plugins.Manifest
 				let bytes = tr.objectStore("bundles").get(cursor.key)
 				bytes.onsuccess = () => {
-					let b = bytes.result
+					let b = bytes.result as Uint8Array
 					let blob = new Blob([b], {type: "application/javascript"})
 					let url = URL.createObjectURL(blob)
 					let activate = async () => {
@@ -145,28 +114,57 @@ async function loadPluginsFromIDB() {
 
 export async function importPlugin(computerFile: File) {
 	let owner = getOwner()
-
-	let gunzip = new DecompressionStream("gzip")
-	let ball = computerFile.stream().pipeThrough(gunzip)
-	let buffer = await new Response(ball).arrayBuffer()
+	let files = await extractPlugin(computerFile.stream())
 	let pluginDatabase = await db
+	storePlugin(pluginDatabase, files)
+	runWithOwner(owner, () => loadPluginsFromIDB(pluginDatabase))
+}
+
+interface TarEntry {
+	/**
+	 * File name
+	 */
+	name: string
+	/**
+	 * File data (don't provide for directories)
+	 */
+	data?: Uint8Array
+}
+
+// todo rather than expecting these specific files, should i just straight-up
+// extract all the bundled files into the indexeddb (or store and load the
+// actual gzipped file and extract it on load?) that way i could handle multiple
+// files, assets, sourcemaps etc in the future... unbundled plugins
+export async function extractPlugin(stream: ReadableStream<Uint8Array>) {
+	let gunzip = new DecompressionStream("gzip")
+	let ball = stream.pipeThrough(gunzip)
+	let buffer = await new Response(ball).arrayBuffer()
 	let files = nanotar.parseTar(buffer)
 	let manifestFile = files.find(file => file.name == "lb.json")
 	let bundleFile = files.find(file => file.name == "bundle.js")
-	let manifest = JSON.parse(new TextDecoder().decode(manifestFile?.data))
+	return {manifest: manifestFile!, bundle: bundleFile!}
+}
+
+export function storePlugin(
+	pluginDatabase: IDBDatabase,
+	files: {
+		manifest: TarEntry
+		bundle: TarEntry
+	},
+) {
+	let manifest = JSON.parse(new TextDecoder().decode(files.manifest.data))
 	let tr = pluginDatabase.transaction(["manifests", "bundles"], "readwrite", {
 		durability: "strict",
 	})
 	tr.objectStore("manifests").put(manifest, manifest.name)
-	tr.objectStore("bundles").put(bundleFile?.data, manifest.name)
-
-	runWithOwner(owner, () => loadPluginsFromIDB())
+	tr.objectStore("bundles").put(files.bundle.data, manifest.name)
 }
 
 export default async function startPlugins() {
 	let owner = getOwner()
 	registerDefaultViews()
+	let pluginDatabase = await db
 	await downloadPluginsFromPluginServer("/")
-		.then(() => runWithOwner(owner, () => loadPluginsFromIDB()))
-		.catch(() => runWithOwner(owner, () => loadPluginsFromIDB()))
+		.then(() => runWithOwner(owner, () => loadPluginsFromIDB(pluginDatabase)))
+		.catch(() => runWithOwner(owner, () => loadPluginsFromIDB(pluginDatabase)))
 }
