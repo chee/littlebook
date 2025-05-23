c613a0fe532221b7b55c2f837d9d20a12346d76b
chee <yay@chee.party>
2025-05-23 22:29:14 +0100

    more elaborate

--- plugins/repl/entry.ts
+++
@@ -1,6 +0,0 @@
-import type PluginAPI from ":/plugins/plugin-api.ts"
-import repl from "./repl.ts"
-
-export default function ReplPlugin(api: PluginAPI) {
-	api.registerView(repl)
-}
--- plugins/tldraw/shared.ts
+++
@@ -1,5 +0,0 @@
-import type {TldrawFile as TldrawFileInterface, TLRecord} from "tldraw"
-export type TldrawFile = TldrawFileInterface &
-	Record<string, any> & {
-		records: TLRecord[]
-	}
