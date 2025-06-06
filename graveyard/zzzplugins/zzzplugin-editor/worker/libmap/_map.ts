88da6e45ebbcb9e2b95c56637f2e8fd567f21215
chee <yay@chee.party>
2025-06-06 21:04:48 +0100

    fix: become immortal

--- graveyard/zzzplugins/zzzplugin-editor/worker/libmap/_map.ts
+++
@@ -1,7 +0,0 @@
-const declarations = import.meta.glob("./*.d.ts", {eager: true, query: "raw"})
-export default new Map(
-	Object.entries(declarations).map(([key, value]) => [
-		key.slice(1),
-		(value as {default: string}).default,
-	]),
-)
